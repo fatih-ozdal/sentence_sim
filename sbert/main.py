b80ec704d2be356f4cd323422bf5d5ede28d335f
Fatih Özdal <141767198+fatih-ozdal@users.noreply.github.com>
2025-01-31 16:12:11 +0300

    Replaced model.similarity with torch's function

--- sbert/main.py
+++ sbert/main.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer
+import torch
 
 def main():
     sentences = [
@@ -18,17 +19,22 @@ def main():
 def load_model(model_name: str) -> SentenceTransformer:
     return SentenceTransformer(model_name)
 
-def encode_sentences(model: SentenceTransformer, sentences: list) -> list:
-    return model.encode(sentences)
+def encode_sentences(model: SentenceTransformer, sentences: list) -> torch.tensor:
+    return torch.tensor(model.encode(sentences))
 
-def find_most_similar_pair(model: SentenceTransformer, sentence_vectors: list) -> tuple:
+def find_most_similar_pair(model: SentenceTransformer, sentence_vectors: torch.tensor) -> tuple:
     max_similarity = 0
     most_similar_pair = (-1, -1)
     
     num_vectors = len(sentence_vectors)
     for i in range(num_vectors - 1):
         for j in range(i + 1, num_vectors):
-            similarity_score = model.similarity([sentence_vectors[i]], [sentence_vectors[j]])[0][0]
+            similarity_score = torch.nn.functional.cosine_similarity(
+                sentence_vectors[i].unsqueeze(0),
+                sentence_vectors[j].unsqueeze(0),
+                dim=1
+            ).item()
+
             if similarity_score > max_similarity:
                 max_similarity = similarity_score
                 most_similar_pair = (i, j)
